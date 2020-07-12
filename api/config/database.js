4acc448253d2081070db70aa6bbfeb22e4bacf2c
Giovani Moutinho <giovanim@ymail.com>
2020-07-11 21:51:21 -0300

    Corrige conexão da API com BD

--- api/config/database.js
+++ api/config/database.js
@@ -4,15 +4,10 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST", "127.0.0.1"),
-        srv: env.bool("DATABASE_SRV", false),
-        port: env.int("DATABASE_PORT", 27017),
+        uri: env("DATABASE_URI", "127.0.0.1"),
         database: env("DATABASE_NAME", "mocorona"),
-        username: env("DATABASE_USERNAME", ""),
-        password: env("DATABASE_PASSWORD", ""),
       },
       options: {
-        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
         ssl: env.bool("DATABASE_SSL", false),
       },
     },
