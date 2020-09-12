980b66852bcae605cbb9b157018afc3faa67615f
Giovani Moutinho <giovanim@ymail.com>
2020-09-12 10:31:29 -0300

    Corrige dict de erros extrator

--- extrator/extratores/bairros.py
+++ extrator/extratores/bairros.py
@@ -20,7 +20,10 @@ class ExtratorBairros:
 
 
     def _texto_tipo_bairros(self, texto):
-        return 'CASOS CONFIRMADOSQORBAIRRO-' in texto
+        return any([
+            'CASOS CONFIRMADOS POR BAIRRO' in texto,
+            'endereços informados no ato' in texto,
+        ])
 
     def _extrai_dados_arquivo(self, texto):
         dados_iniciais = re.findall(r'(?P<bairro>[\w\d .?-]+)\t(?P<casos>\d+)', texto)
@@ -38,14 +41,20 @@ class ExtratorBairros:
     def _substitui_erros_comuns(self, texto):
         substituicoes = {
             '11': 'II',
-            'SR?': 'SR.ª',
-            'ANTÔNIO 1': 'ANTÔNIO I',
-            'ANÂLIA': 'ANÁLIA',
-            'SR3.': 'SR.ª',
+            'ID,': 'JD.',
+            'ID.': 'JD.',
+            'Nê': 'Nª',
+            'SP MARIA': 'STª MARIA',
             'SR*': 'SR.ª',
             'SR-': 'SR.ª',
-            '1 BITU RU NA': 'IBITURUNA',
-            'CARME LO': 'CARMELO',
+            'SR3.': 'SR.ª',
+            'SR?': 'SR.ª',
+            'ST2': 'STO',
+            'ST?': 'STª',
+            'STP': 'STO',
+            'STê': 'STª',
+            'SV': 'STª',
+            'VI.': 'VL.',
         }
         for erro in substituicoes.keys():
             texto = texto.replace(erro, substituicoes[erro])
