cb166b0900ca9255285362b1fc882b9b1d364a2a
Giovani Moutinho <giovanim@ymail.com>
2021-11-05 10:41:30 -0300

    Add VaccineSummaries handlers

--- api/handlers/handlers.go
+++ api/handlers/handlers.go
@@ -42,3 +42,31 @@ func (api *APIEnv) CreateCovidSummary(c *gin.Context) {
 	}
 	c.JSON(http.StatusCreated, summary)
 }
+
+func (api *APIEnv) GetVaccineSummaries(c *gin.Context) {
+	summaries, err := database.GetVaccineSummaries(api.DB)
+	if err != nil {
+		log.Print(err)
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, summaries)
+}
+
+func (api *APIEnv) CreateVaccineSummary(c *gin.Context) {
+	summary := models.VaccineSummary{}
+	err := c.BindJSON(&summary)
+	if err != nil {
+		log.Print(err)
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	summary, err = database.CreateVaccineSummary(api.DB, summary)
+	if err != nil {
+		log.Print(err)
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusCreated, summary)
+}
