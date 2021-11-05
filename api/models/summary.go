94074b3718e7ffb124b133ca12f87566de6e3528
Giovani Moutinho <giovanim@ymail.com>
2021-11-05 10:36:13 -0300

    Create Summaries models

---
+++ api/models/summary.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
+type CovidSummary struct {
+	gorm.Model
+	Notification int
+	Negative     int
+	Confirmed    int
+	Recovered    int
+	Dead         int
+	Release      time.Time
+}
+
+type VaccineSummary struct {
+	gorm.Model
+	UniqueDose  int
+	FirstDose   int
+	SecondDose  int
+	BoosterDose int
+	Release     time.Time
+}
