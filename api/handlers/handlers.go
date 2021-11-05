00945271dde38d72e60269204a3db15f4650a58a
Giovani Moutinho <giovanim@ymail.com>
2021-11-05 10:35:54 -0300

    Create CovidSummaries handlers

---
+++ api/handlers/handlers.go
@@ -0,0 +1,44 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+
+	"mocorona-api/database"
+	"mocorona-api/models"
+)
+
+type APIEnv struct {
+	DB *gorm.DB
+}
+
+func (api *APIEnv) GetCovidSummaries(c *gin.Context) {
+	summaries, err := database.GetCovidSummaries(api.DB)
+	if err != nil {
+		log.Print(err)
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, summaries)
+}
+
+func (api *APIEnv) CreateCovidSummary(c *gin.Context) {
+	summary := models.CovidSummary{}
+	err := c.BindJSON(&summary)
+	if err != nil {
+		log.Print(err)
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	summary, err = database.CreateCovidSummary(api.DB, summary)
+	if err != nil {
+		log.Print(err)
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusCreated, summary)
+}
