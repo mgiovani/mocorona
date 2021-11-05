02c601e583e62a246a2bb34b9c22f4088eec5ed6
Giovani Moutinho <giovanim@ymail.com>
2021-11-05 10:35:38 -0300

    Crate database structure

---
+++ api/database/db.go
@@ -0,0 +1,56 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+
+	"mocorona-api/models"
+)
+
+var DB *gorm.DB
+
+func Setup() {
+	host := "localhost"
+	port := "5432"
+	dbname := "mocorona"
+	user := "postgres"
+	password := "abc123"
+
+	db, err := gorm.Open("postgres", fmt.Sprintf("host=%s "+
+		"port=%s dbname=%s user=%s password=%s sslmode=disable",
+		host, port, dbname, user, password))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.LogMode(false)
+	db.AutoMigrate([]models.CovidSummary{}, []models.VaccineSummary{})
+	DB = db
+}
+
+func GetDB() *gorm.DB {
+	return DB
+}
+
+func GetCovidSummaries(db *gorm.DB) ([]models.CovidSummary, error) {
+	summaries := []models.CovidSummary{}
+	query := db.Select("covid_summaries.*").Group("covid_summaries.id")
+
+	if err := query.Find(&summaries).Error; err != nil {
+		return summaries, err
+	}
+	return summaries, nil
+}
+
+func CreateCovidSummary(
+	db *gorm.DB, summary models.CovidSummary) (models.CovidSummary, error) {
+
+	if err := db.Create(&summary).Error; err != nil {
+		return summary, err
+	}
+	return summary, nil
+}
